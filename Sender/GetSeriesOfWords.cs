2b728770897382a25a2865253fccbc8658ed8442
khushi choudhary <khushichoudhary12599@gmail.com>
2020-09-16 19:38:49 +0530

    GetSeriousofWord and OutputToconsole File added

---
+++ Sender/GetSeriesOfWords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    public class GetSeriesOfWords
+    {
+        public static void ConvertCommentsToWords(string path)
+        {
+            var ReadingCsvFile = new StreamReader(File.OpenRead(@"D:\Review_Csv_File.csv"));
+            List<string> DateTimeColumn = new List<string>();
+            List<string> CommentColumn = new List<string>();
+
+            while (!ReadingCsvFile.EndOfStream)
+            {
+                var StoreDataEachRowOfCsvFile = ReadingCsvFile.ReadLine();
+                var SplitColumn = StoreDataEachRowOfCsvFile.Split(',', '\n');
+                DateTimeColumn.Add(SplitColumn[0]);
+                CommentColumn.Add(SplitColumn[1]);
+            }
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (string CommentData in CommentColumn)
+            {
+                builder.Append(CommentData);
+            }
+
+            string Comment = builder.ToString();
+            string[] word = Comment.Split(' ');
+            OutputToConsole.WordsDisplayOnConsole(word);
+        }
+    }
+}
---
+++ Sender/OutputToConsole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Sender
+{
+    public class OutputToConsole
+    {
+        public static void WordsDisplayOnConsole(string[] words)
+        {
+            for (int i = 0; i <words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
