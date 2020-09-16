e0b64f3fc56dab8b4c3f5a52494dd1b90ebf74eb
Rahul Hangal <49347697+rahulshhangal@users.noreply.github.com>
2020-09-16 19:30:44 +0530

    Sender and Solution Changes

---
+++ Sender/CsvFileReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace Sender
+{
+    public class CsvFileReader
+    {
+        public static bool CheckAndReadCsvFile(string path)
+        {
+            bool pathExists = CheckPathExistOrNot(path);
+            bool val = false;
+            if (pathExists == true)
+            {
+                GetSeriesOfWords.ConvertCommentsToWords(path);
+                val = true;
+                return val;
+            }
+            else
+            {
+                return val;
+            }
+        }
+        public static bool CheckPathExistOrNot(string path)
+        {
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+                return false;
+            }
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:/Review_Csv_File.csv";
+            CsvFileReader.CheckAndReadCsvFile(path);
+        }
+    }
+}
