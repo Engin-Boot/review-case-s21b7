fa81e7fbee6c4ebc7e95f6472941bd1a56d2de31
khushi choudhary <khushichoudhary12599@gmail.com>
2020-09-17 17:22:39 +0530

    WordAndwordCount and WriteToCsv file added to receiver

---
+++ Receiver/WordAndWordCount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    public class WordAndWordCount
+    {
+        public static Dictionary<string,int> WordsAndFrequency(string[] words)
+        {
+            Dictionary<String, int> wordAndWordFrequency = new Dictionary<String, int>();
+            try
+            {
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (wordAndWordFrequency.ContainsKey(words[i]))
+                    {
+                        wordAndWordFrequency[words[i]] = wordAndWordFrequency[words[i]] + 1;
+                    }
+                    else
+                    {
+                        wordAndWordFrequency.Add(words[i], 1);
+                    }
+                }
+                Console.WriteLine("Word Frequency Count Successfull");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Word and Word Counting Error: " + e);
+            }
+
+            return wordAndWordFrequency;
+        }
+        
+    }
+}
---
+++ Receiver/WriteToCsvFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    public class WriteToCsvFile
+    {
+        public static void RedirectToCsvFile(Dictionary<String, int> wordAndWordFrequency)
+        {
+            try
+            {
+                using (var writeToCsvFile = new StreamWriter(@"D:\Write.csv"))
+                {
+                    foreach (KeyValuePair<string, int> wordisKeyWorkFrequencyisValue in wordAndWordFrequency)
+                    {
+                        writeToCsvFile.WriteLine("{0} , {1}", wordisKeyWorkFrequencyisValue.Key, wordisKeyWorkFrequencyisValue.Value);
+                    }
+                }
+                Console.WriteLine("Writting To File Successfull");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro While Writting To File: " + e);
+            }
+            
+        }
+        
+    }
+}
